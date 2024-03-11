6bc3de1dcdd7a13e423d7bbacac7e56a3e4fd7df
Henry Le <HLe@rosen-group.com>
2024-03-11 13:19:17 +0700

    Add project files.

---
+++ LMCB_TestForm/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LMCB_TestForm
+{
+    public partial class Form1 : Form
+    {
+
+        private TcpClient tcpClient;
+        private StreamReader sReader;
+        private StreamWriter sWriter;
+        private Thread clientThread;
+        private int serverPort = 8000;
+        private bool stopTcpClient = true;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ClientRecv()
+        {
+            StreamReader sr = new StreamReader(tcpClient.GetStream());
+            try
+            {
+                while (!stopTcpClient && tcpClient.Connected)
+                {
+                    Application.DoEvents();
+                    string data = sr.ReadLine();
+                    UpdateChatHistoryThreadSafe($"{data}\n");
+                }
+            }
+            catch (SocketException sockEx)
+            {
+                tcpClient.Close();
+                sr.Close();
+
+            }
+        }
+
+        private delegate void SafeCallDelegate(string text);
+
+        private void UpdateChatHistoryThreadSafe(string text)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(UpdateChatHistoryThreadSafe);
+                richTextBox1.Invoke(d, new object[] {text});
+            }
+            else
+            {
+                richTextBox1.Text += text;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sWriter.WriteLine($"{textBox3.Text};{sendMsgTextBox.Text}");
+                sendMsgTextBox.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Close(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                stopTcpClient = false;
+
+                this.tcpClient = new TcpClient();
+                this.tcpClient.Connect(new IPEndPoint(IPAddress.Parse(textBox2.Text), serverPort));
+                this.sWriter = new StreamWriter(tcpClient.GetStream());
+                this.sWriter.AutoFlush = true;
+                sWriter.WriteLine(this.textBox1.Text);
+                clientThread = new Thread(this.ClientRecv);
+                clientThread.Start();
+                MessageBox.Show("Connected");
+            }
+            catch (SocketException sockEx)
+            {
+                MessageBox.Show(sockEx.Message, "Network error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ ServerChatForm/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerChatForm
+{
+    public partial class Form1 : Form
+    {
+        private Thread listenThread;
+        private TcpListener tcpListener;
+        private bool stopChatServer = true;
+        private readonly int _serverPort = 8000;
+        private Dictionary<string,TcpClient> dict = new Dictionary<string,TcpClient>();
+        private Thread replyThread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(new IPEndPoint(IPAddress.Parse(textBox1.Text), _serverPort));
+                tcpListener.Start();
+
+                while (!stopChatServer)
+                {
+                    //Application.DoEvents();
+                    TcpClient _client = tcpListener.AcceptTcpClient();
+
+                    StreamReader sr = new StreamReader(_client.GetStream());
+                    StreamWriter sw = new StreamWriter(_client.GetStream());
+                    sw.AutoFlush = true;
+                    string username = sr.ReadLine();
+                    if (string.IsNullOrEmpty(username))
+                    {
+                        sw.WriteLine("Please pick a username");
+                        _client.Close();
+                    }
+                    else
+                    {
+                        if (!dict.ContainsKey(username))
+                        {
+                            Thread clientThread = new Thread(() => this.ClientRecv(username, _client));
+                            dict.Add(username, _client);
+                            clientThread.Start();
+                        }
+                        else
+                        {
+                            sw.WriteLine("Username already exist, pick another one");
+                            _client.Close();
+                        }
+                    }
+
+                }
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void ClientRecv(string username, TcpClient tcpClient)
+        {
+            StreamReader sr = new StreamReader(tcpClient.GetStream());
+            try
+            {
+                while (!stopChatServer)
+                {
+                    Application.DoEvents();
+                    string userNameAndMsg = sr.ReadLine();
+                    string[] arrPayload = userNameAndMsg.Split(';');
+                    string friendUsername = arrPayload[0];
+                    string msg = arrPayload[1];
+                    string formattedMsg = $"[{DateTime.Now:MM/dd/yyyy h:mm tt}] {username}: {msg}\n";
+                    TcpClient friendTcpClient;
+
+                    if (dict.TryGetValue(friendUsername, out friendTcpClient))
+                    {
+                        StreamWriter sw = new StreamWriter(friendTcpClient.GetStream());
+                        sw.WriteLine(formattedMsg);
+                        sw.AutoFlush = true;
+                    }
+                    StreamWriter sw2 = new StreamWriter(tcpClient.GetStream());
+                    sw2.WriteLine(formattedMsg);
+                    sw2.AutoFlush = true;
+                    //                    foreach (TcpClient otherClient in dict.Values)
+                    //                    {
+                    //                        StreamWriter sw = new StreamWriter(otherClient.GetStream());
+                    //                        sw.WriteLine(formattedMsg);
+                    //                        sw.AutoFlush = true;
+                    //
+                    //                    }
+
+                    UpdateChatHistoryThreadSafe(formattedMsg);
+                }
+            }
+            catch (SocketException sockEx)
+            {
+                tcpClient.Close();
+                sr.Close();
+
+            }
+
+        }
+        private delegate void SafeCallDelegate(string text);
+
+        private void UpdateChatHistoryThreadSafe(string text)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(UpdateChatHistoryThreadSafe);
+                richTextBox1.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                richTextBox1.Text += text;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (stopChatServer)
+            {
+                stopChatServer = false;
+                listenThread = new Thread(this.Listen);
+                listenThread.Start();
+                MessageBox.Show(@"Start listening for incoming connections");
+                button1.Text = @"Stop";
+            }
+            else
+            {
+                stopChatServer = true;
+                button1.Text = @"Start listening";
+                tcpListener.Stop();
+                listenThread = null;
+               
+            }
+        }
+    }
+}
